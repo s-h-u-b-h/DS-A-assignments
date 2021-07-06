401b84b7d6f3397bf3977f260e556cc1c7fe5352
Shubham Pathaniya <68888348+s-h-u-b-h@users.noreply.github.com>
2021-07-06 12:38:40 +0530

    Update q4.cpp

--- Day2/q4.cpp
+++ Day2/q4.cpp
@@ -1,3 +1,6 @@
+  /*Given an array of integers nums.
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+Return the number of good pairs.*/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
